0925eb2a21153050e8430d1d9a5eb03acd31aa34
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2024-09-25 22:30:40 +0300

    prepopulated inventory closes #5

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO inventory (description, name, price) VALUES ('for writing', 'pencil', 5000);
+INSERT INTO inventory (description, name, price) VALUES ('for ruling', 'ruler', 10000);
+INSERT INTO inventory (description, name, price) VALUES ('for erasing', 'eraser', 2000);
+INSERT INTO inventory (description, name, price) VALUES ('for drawing', 'drawing_pad', 7000);
+INSERT INTO inventory (description, name, price) VALUES ('for taking notes', 'book', 6000);
+INSERT INTO inventory (description, name, price) VALUES ('for packing lunch', 'lunch box', 8000);
